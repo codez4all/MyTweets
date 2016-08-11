5c00a321e3ea1db4fad9001bbc8bdae50324839a
Sr <slickpay2014+sr@gmail.com>
2016-08-11 15:47:49 -0700

    Endless scroll functionality added

--- app/src/main/java/com/codepath/apps/mytweets/activity/TimelineActivity.java
+++ app/src/main/java/com/codepath/apps/mytweets/activity/TimelineActivity.java
@@ -3,16 +3,20 @@ package com.codepath.apps.mytweets.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ListView;
 
+import com.codepath.apps.mytweets.EndlessRecyclerViewScrollListener;
 import com.codepath.apps.mytweets.R;
 import com.codepath.apps.mytweets.TwitterApplication;
 import com.codepath.apps.mytweets.TwitterClient;
 import com.codepath.apps.mytweets.adapter.TweetsArrayAdapter;
+import com.codepath.apps.mytweets.adapter.TweetsRecycleAdapter;
 import com.codepath.apps.mytweets.models.Tweet;
 import com.loopj.android.http.JsonHttpResponseHandler;
 
@@ -30,6 +34,10 @@ public class TimelineActivity extends AppCompatActivity {
     private ArrayList<Tweet> tweets;
     private ListView lvTweets;
     private final int REQUEST_CODE = 200;
+    private RecyclerView recyclerView;
+    private TweetsRecycleAdapter recycleAdapter;
+
+    private long lastSince_Id = 0;
 
 
     @Override
@@ -38,23 +46,44 @@ public class TimelineActivity extends AppCompatActivity {
         setContentView(R.layout.activity_timeline);
 
        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+
         setSupportActionBar(toolbar);
 
         //find listview
-        lvTweets = (ListView)findViewById(R.id.lvTweets);
+       // lvTweets = (ListView)findViewById(R.id.lvTweets);
 
         //Create arraylist
         tweets = new ArrayList<>();
 
         //Create adapter
-        aTweets = new TweetsArrayAdapter(this.getApplicationContext(),tweets);
+      //  aTweets = new TweetsArrayAdapter(this.getApplicationContext(),tweets);
 
         //bind arraylist to listview using adapter
-        lvTweets.setAdapter(aTweets);
+       //  lvTweets.setAdapter(aTweets);
 
         client = TwitterApplication.getRestClient(); //singletone client
 
-        populateTimeline();
+
+
+        //endless scrolling using Recycleview
+        recyclerView = (RecyclerView)findViewById(R.id.recycleView);
+
+        recycleAdapter = new TweetsRecycleAdapter(this.getApplicationContext(),tweets);
+        recyclerView.setAdapter(recycleAdapter);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        recyclerView.addOnScrollListener(new EndlessRecyclerViewScrollListener(linearLayoutManager) {
+
+            @Override
+            public void onLoadMore(int page, int totalItemsCount) {
+                customLoadMoreDataFromApi(page++);
+            }
+
+        });
+
+        populateTimeline(1);
     }
 
     @Override
@@ -95,16 +124,20 @@ public class TimelineActivity extends AppCompatActivity {
             Tweet tweet = (Tweet)data.getSerializableExtra("newTweet");
             int code = data.getExtras().getInt("code", 0);
 
-            // tweets.add(0,tweet);
-            aTweets.insert(tweet,0);
-            aTweets.notifyDataSetChanged();
+            tweets.add(0,tweet);
+
+           // aTweets.insert(tweet,0);
+           // aTweets.notifyDataSetChanged();
+
+            recycleAdapter.notifyItemInserted(0);
         }
     }
 
     //send api request to get timeline Json
    // fill the ListView by creating tweet objects from Json.
-    private void populateTimeline() {
+    private void populateTimeline(int page) {
 
+        Log.d("DEBUG","Page: "+ page);
         client.getHomeTimeline(new JsonHttpResponseHandler(){
 
             //Success
@@ -113,11 +146,30 @@ public class TimelineActivity extends AppCompatActivity {
                 super.onSuccess(statusCode, headers, json);
                 //Deserialize Json, Create models and load model data into listview
 
-                tweets = Tweet.fromJsonArray(json);
-                aTweets.addAll(tweets);
-                aTweets.notifyDataSetChanged();
+                ArrayList<Tweet> arrayList = Tweet.fromJsonArray(json);
 
-                Log.d("DEBUG", aTweets.toString());
+                tweets.addAll(arrayList);
+                Log.d("DEBUG","getHomeTimeline tweets:"+ tweets.toString());
+
+
+                //  aTweets.addAll(tweets);
+              //  aTweets.notifyDataSetChanged();
+                int curSize = recycleAdapter.getItemCount();
+                Log.d("DEBUG", "Recycle adapter Size before:" + curSize);
+
+                recycleAdapter.notifyItemRangeInserted(curSize, tweets.size());
+
+                Log.d("DEBUG", "Recycle adapter Size After:" + recycleAdapter.getItemCount());
+                Log.d("DEBUG","getHomeTimeline with recycleAdapter:"+ recycleAdapter.toString());
+
+
+                //get last tweet's id
+               /* if(tweets.size() > 0) {
+                    lastSince_Id = arrayList.get((arrayList.size() - 1)).getUid();
+                    Log.d("DEBUG","tweet size:"+arrayList.size());
+                    Log.d("DEBUG","last since_id:"+lastSince_Id);
+
+                }*/
 
 
             }
@@ -129,7 +181,17 @@ public class TimelineActivity extends AppCompatActivity {
 
                 Log.d("DEBUG", errorResponse.toString());
             }
-        });
+        },page);
+
+    }
+
+    public void customLoadMoreDataFromApi(int page) {
+        // Send an API request to retrieve appropriate data using the offset value as a parameter.
+        //  --> Deserialize API response and then construct new objects to append to the adapter
+        //  --> Notify the adapter of the changes
+
+        populateTimeline(page);
+
     }
 
 }
