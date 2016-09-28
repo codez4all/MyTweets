5d1785b23a7358a8a44f90bfad941a856d90a11e
Sheetal <sheetalw25@gmail.com>
2016-09-28 08:19:25 -0700

    UI Style improvements

--- app/src/main/java/com/codepath/apps/mytweets/ParseRelativeDate.java
+++ app/src/main/java/com/codepath/apps/mytweets/ParseRelativeDate.java
@@ -4,6 +4,7 @@ import android.text.format.DateUtils;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Locale;
 
 
@@ -30,6 +31,44 @@ public class ParseRelativeDate {
             relativeDate = DateUtils.getRelativeTimeSpanString(dateMillis,
                     System.currentTimeMillis(), DateUtils.SECOND_IN_MILLIS).toString();
         } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        return relativeDate;
+    }
+
+    public static String getFormattedRelativeTime(String rawJsonDate){
+
+
+        String twitterFormat = "EEE MMM dd HH:mm:ss ZZZZZ yyyy";
+        SimpleDateFormat sf = new SimpleDateFormat(twitterFormat, Locale.ENGLISH);
+        sf.setLenient(true);
+
+        String relativeDate = "";
+
+        try {
+
+            Date timestamp = sf.parse(rawJsonDate);
+
+            String relativeTime = DateUtils.getRelativeTimeSpanString(timestamp.getTime()).toString();
+
+            String[] words = relativeTime.split("\\s+");
+            if(relativeTime.startsWith("in")){
+                relativeDate = "new";
+            }else{
+                if(words.length > 1){
+                    if(words[0].length() > 1){
+                        relativeDate = String.format("%s %s", words[0], words[1].charAt(0));
+                    }else{
+                        relativeDate = String.format("%s%s", words[0], words[1].charAt(0));
+                    }
+
+                }else{
+                    relativeDate = words[0];
+                }
+            }
+
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
--- app/src/main/java/com/codepath/apps/mytweets/adapter/TweetsRecycleAdapter.java
+++ app/src/main/java/com/codepath/apps/mytweets/adapter/TweetsRecycleAdapter.java
@@ -12,6 +12,7 @@ import com.codepath.apps.mytweets.ParseRelativeDate;
 import com.codepath.apps.mytweets.R;
 import com.codepath.apps.mytweets.models.Tweet;
 import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Transformation;
 
 import java.util.List;
 
@@ -84,12 +85,19 @@ public class TweetsRecycleAdapter extends RecyclerView.Adapter<TweetsRecycleAdap
         viewHolder.tvUserName.setText(tweet.getUser().getScreenName().toString());
 
        // TextView tTimeStamp = viewHolder.tvTimestamp;
-        String relativeDate = ParseRelativeDate.getRelativeTimeAgo(tweet.getCreatedAt().toString());
-        viewHolder.tvTimestamp.setText(relativeDate.toString());
+        String relativeDate = ParseRelativeDate.getFormattedRelativeTime(tweet.getCreatedAt());
+        viewHolder.tvTimestamp.setText(relativeDate);
 
        // ImageView ivProfile = viewHolder.ivProfileImage;
         viewHolder.ivProfileImage.setImageResource(0);
-        Picasso.with(this.getContext()).load(tweet.getUser().getProfileImageUrl()).into(viewHolder.ivProfileImage);
+
+        final int radius = 5;
+        final int margin = 0;
+        final Transformation transformation = new RoundedCornersTransformation(radius, margin);
+        Picasso.with(this.getContext())
+            .load(tweet.getUser().getProfileImageUrl())
+            .transform(transformation)
+            .into(viewHolder.ivProfileImage);
 
     }
 
